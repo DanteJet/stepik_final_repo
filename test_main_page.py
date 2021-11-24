251d516b767f471fc0a3723b507b826bca431f0e
Yakov Bubnov <y.bubnov@ispsystem.com>
2021-11-24 17:36:08 +0800

    delete unused strings

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,3 @@
-import time
 from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
@@ -6,8 +5,6 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    #login_page = page.go_to_login_page()
-    #login_page.should_be_login_link()
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
